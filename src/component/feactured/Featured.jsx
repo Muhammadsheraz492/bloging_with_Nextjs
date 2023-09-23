8c586b2ff6d3eb0a7a68e1963c90bdffdf015b6a
Muhammad Sheraz <90733116+Muhammadsheraz492@users.noreply.github.com>
2023-09-23 23:03:48 +0500

    Start working on Feactured Project

--- src/component/feactured/Featured.jsx
+++ src/component/feactured/Featured.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 import style from './featured.module.css'
+import Image from 'next/image'
 export const Featured = () => {
   return (
-    <div  className={style.container}>featured</div>
+    <div  className={style.container}>
+    <div  className={style.title}>
+   <h1><b>Hey, Muhammad Shiraz here!</b> Discover my Stories and creative ideas</h1>
+    </div>
+   <div  className={style.post}>
+    <div  className={style.imgContainer}>
+      <Image  src={"/p1.jpeg"} alt="" fill/>
+    </div>
+    <div  className={style.textContainer}>
+      
+    </div>
+   </div>
+
+    </div>
   )
 }
